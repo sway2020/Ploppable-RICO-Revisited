390e9de368617c08a055dcdb23fc5fce550d66fa
algernon <aeneas@aloku.net>
2020-03-22 15:19:28 +1100

    Rename to 'revisited'.

--- PloppableRCI/Mod.cs
+++ PloppableRCI/Mod.cs
@@ -6,8 +6,8 @@ namespace PloppableRICO
 {
     public class PloppableRICOMod : IUserMod
 	{
-		public string Name => "Ploppable RICO";
+		public string Name => "RICO Revisited";
 
-		public string Description => "Allows Plopping of RICO Buildings";
+		public string Description => "Allows Plopping of RICO Buildings (fork of AJ3D's original with bugfixes and new features)";
 	}
 }
