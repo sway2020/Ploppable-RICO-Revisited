090bede463d97a796c225352e4b2a85f7f972346
algernon <aeneas@aloku.net>
2020-04-07 15:54:19 +1000

    Patch CalculateHomeCount on level load. Overrides game HomeCount calculation for loaded RICO buildings.

--- PloppableRCI/Loading.cs
+++ PloppableRCI/Loading.cs
@@ -51,6 +51,8 @@ namespace PloppableRICO
 
         public override void OnLevelLoaded(LoadMode mode)
         {
+            // CalculateHomeCount patch only needed while loading existing buildings; unapply patch now that everything is loaded.
+            _harmony.Unpatch(typeof(ResidentialBuildingAI).GetMethod("CalculateHomeCount"), typeof(RICOHomeCount).GetMethod("Prefix"));
 
             base.OnLevelLoaded(mode);
 
