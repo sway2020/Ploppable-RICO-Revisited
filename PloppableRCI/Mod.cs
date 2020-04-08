7d02e9f430f149d989919df0502876c0bb385db2
algernon <aeneas@aloku.net>
2020-04-08 15:39:59 +1000

    Prepare for 1.2 release.

--- PloppableRCI/Mod.cs
+++ PloppableRCI/Mod.cs
@@ -5,7 +5,7 @@ namespace PloppableRICO
 {
     public class PloppableRICOMod : IUserMod
 	{
-		public static string version = "1.1.1";
+		public static string version = "1.2";
 
 		public string Name => "RICO Revisited " + version;
 
