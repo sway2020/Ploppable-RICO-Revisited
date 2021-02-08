80cff1e6d03f2f0dacc7a4ebbc2aa06cfb73b448
algernon-A <aeneas@aloku.net>
2021-02-08 13:51:07 +1100

    Convert OnLevelLoaded to Harmony postfix.

--- PloppableRCI/Loading.cs
+++ PloppableRCI/Loading.cs
@@ -1,7 +1,6 @@
 using System.IO;
 using System.Collections.Generic;
 using ICities;
-using PloppableRICO.MessageBox;
 
 
 namespace PloppableRICO
@@ -24,13 +23,13 @@ namespace PloppableRICO
         internal static PloppableRICODefinition mod2RicoDef;
 
         // Internal flags.
-        private static bool isModEnabled = false;
-        private bool harmonyLoaded = false;
+        internal static bool isModEnabled = false;
+        internal static bool harmonyLoaded = false;
         internal static bool patchOperating = false;
 
         // Used to flag if conflicting mods are running.
-        private static bool conflictingMod = false;
-        private static bool softModConflct;
+        internal static bool conflictingMod = false;
+        internal static bool softModConflct;
 
 
         /// <summary>
@@ -129,102 +128,6 @@ namespace PloppableRICO
             }
         }
 
-
-        /// <summary>
-        /// Called by the game when level loading is complete.
-        /// </summary>
-        /// <param name="mode">Loading mode (e.g. game, editor, scenario, etc.)</param>
-        public override void OnLevelLoaded(LoadMode mode)
-        {
-            base.OnLevelLoaded(mode);
-
-            // Wait for loading to fully complete.
-            while (!LoadingManager.instance.m_loadingComplete) { }
-
-            // Check watchdog flag.
-            if (!patchOperating)
-            {
-                // Patch wasn't operating; display harmony error and abort.
-                harmonyLoaded = false;
-                isModEnabled = false;
-            }
-
-            // Check to see that Harmony 2 was properly loaded.
-            if (!harmonyLoaded)
-            {
-                // Harmony 2 wasn't loaded; display warning notification and exit.
-                ListMessageBox harmonyBox = MessageBoxBase.ShowModal<ListMessageBox>();
-
-                // Key text items.
-                harmonyBox.AddParas(Translations.Translate("ERR_HAR0"), Translations.Translate("PRR_ERR_HAR"), Translations.Translate("PRR_ERR_FAT"), Translations.Translate("ERR_HAR1"));
-
-                // List of dot points.
-                harmonyBox.AddList(Translations.Translate("ERR_HAR2"), Translations.Translate("ERR_HAR3"));
-
-                // Closing para.
-                harmonyBox.AddParas(Translations.Translate("MES_PAGE"));
-            }
-
-            // Check to see if a conflicting mod has been detected.
-            if (conflictingMod)
-            {
-                // Mod conflict detected - display warning notification and exit.
-                ListMessageBox modConflictBox = MessageBoxBase.ShowModal<ListMessageBox>();
-
-                // Key text items.
-                modConflictBox.AddParas(Translations.Translate("ERR_CON0"), Translations.Translate("PRR_ERR_FAT"), Translations.Translate("PRR_ERR_CON0"), Translations.Translate("ERR_CON1"));
-
-                // Add conflicting mod name(s).
-                modConflictBox.AddList(ModUtils.conflictingModNames.ToArray());
-
-                // Closing para.
-                modConflictBox.AddParas(Translations.Translate("PRR_ERR_CON1"));
-            }
-
-            // Don't do anything further if mod hasn't activated for whatever reason (mod conflict, harmony error, something else).
-            if (!isModEnabled)
-            {
-                return;
-            }
-
-            // Report any 'soft' mod conflicts.
-            if (softModConflct)
-            {
-                // Soft conflict detected - display warning notification for each one.
-                foreach (string mod in ModUtils.conflictingModNames)
-                {
-                    if (mod.Equals("PTG") && ModSettings.dsaPTG == 0)
-                    {
-                        // Plop the Growables.
-                        DontShowAgainMessageBox softConflictBox = MessageBoxBase.ShowModal<DontShowAgainMessageBox>();
-                        softConflictBox.AddParas(Translations.Translate("PRR_CON_PTG0"), Translations.Translate("PRR_CON_PTG1"), Translations.Translate("PRR_CON_PTG2"));
-                        softConflictBox.DSAButton.eventClicked += (component, clickEvent) => { ModSettings.dsaPTG = 1; SettingsUtils.SaveSettings(); };
-                    }
-                }
-            }
-
-            // Report any broken assets and remove from our prefab dictionary.
-            foreach (BuildingInfo prefab in brokenPrefabs)
-            {
-                Logging.Error("broken prefab: ", prefab.name);
-                xmlManager.prefabHash.Remove(prefab);
-            }
-            brokenPrefabs.Clear();
-
-            // Init Ploppable Tool panel.
-            PloppableTool.Initialize();
-
-            // Add buttons to access building details from zoned building info panels.
-            SettingsPanel.AddInfoPanelButtons();
-
-            Logging.KeyMessage("loading complete");
-
-            // Display update notification.
-            WhatsNew.ShowWhatsNew();
-
-            // Set up options panel event handler.
-            OptionsPanel.OptionsEventHook();
-
-        }
+        // OnLevelLoaded has been replaced with Harmony Postfix.
     }
 }
---
+++ PloppableRCI/Patches/OnLevelLoaded.cs
@@ -0,0 +1,104 @@
+﻿using HarmonyLib;
+using PloppableRICO.MessageBox;
+
+
+namespace PloppableRICO
+{
+    /// <summary>
+    /// Harmony Postfix patch for OnLevelLoaded.  This enables us to perform setup tasks after all loading has been completed.
+    /// </summary>
+    [HarmonyPatch(typeof(LoadingWrapper))]
+    [HarmonyPatch("OnLevelLoaded")]
+    public static class OnLevelLoadedPatch
+    {
+        /// <summary>
+        /// Harmony postfix to perform actions require after the level has loaded.
+        /// </summary>
+        public static void Postfix()
+        {
+            // Check watchdog flag.
+            if (!Loading.patchOperating)
+            {
+                // Patch wasn't operating; display harmony error and abort.
+                Loading.harmonyLoaded = false;
+                Loading.isModEnabled = false;
+            }
+
+            // Check to see that Harmony 2 was properly loaded.
+            if (!Loading.harmonyLoaded)
+            {
+                // Harmony 2 wasn't loaded; display warning notification and exit.
+                ListMessageBox harmonyBox = MessageBoxBase.ShowModal<ListMessageBox>();
+
+                // Key text items.
+                harmonyBox.AddParas(Translations.Translate("ERR_HAR0"), Translations.Translate("PRR_ERR_HAR"), Translations.Translate("PRR_ERR_FAT"), Translations.Translate("ERR_HAR1"));
+
+                // List of dot points.
+                harmonyBox.AddList(Translations.Translate("ERR_HAR2"), Translations.Translate("ERR_HAR3"));
+
+                // Closing para.
+                harmonyBox.AddParas(Translations.Translate("MES_PAGE"));
+            }
+
+            // Check to see if a conflicting mod has been detected.
+            if (Loading.conflictingMod)
+            {
+                // Mod conflict detected - display warning notification and exit.
+                ListMessageBox modConflictBox = MessageBoxBase.ShowModal<ListMessageBox>();
+
+                // Key text items.
+                modConflictBox.AddParas(Translations.Translate("ERR_CON0"), Translations.Translate("PRR_ERR_FAT"), Translations.Translate("PRR_ERR_CON0"), Translations.Translate("ERR_CON1"));
+
+                // Add conflicting mod name(s).
+                modConflictBox.AddList(ModUtils.conflictingModNames.ToArray());
+
+                // Closing para.
+                modConflictBox.AddParas(Translations.Translate("PRR_ERR_CON1"));
+            }
+
+            // Don't do anything further if mod hasn't activated for whatever reason (mod conflict, harmony error, something else).
+            if (!Loading.isModEnabled)
+            {
+                return;
+            }
+
+            // Report any 'soft' mod conflicts.
+            if (Loading.softModConflct)
+            {
+                // Soft conflict detected - display warning notification for each one.
+                foreach (string mod in ModUtils.conflictingModNames)
+                {
+                    if (mod.Equals("PTG") && ModSettings.dsaPTG == 0)
+                    {
+                        // Plop the Growables.
+                        DontShowAgainMessageBox softConflictBox = MessageBoxBase.ShowModal<DontShowAgainMessageBox>();
+                        softConflictBox.AddParas(Translations.Translate("PRR_CON_PTG0"), Translations.Translate("PRR_CON_PTG1"), Translations.Translate("PRR_CON_PTG2"));
+                        softConflictBox.DSAButton.eventClicked += (component, clickEvent) => { ModSettings.dsaPTG = 1; SettingsUtils.SaveSettings(); };
+                    }
+                }
+            }
+
+            // Report any broken assets and remove from our prefab dictionary.
+            foreach (BuildingInfo prefab in Loading.brokenPrefabs)
+            {
+                Logging.Error("broken prefab: ", prefab.name);
+                Loading.xmlManager.prefabHash.Remove(prefab);
+            }
+            Loading.brokenPrefabs.Clear();
+
+            // Init Ploppable Tool panel.
+            PloppableTool.Initialize();
+
+            // Add buttons to access building details from zoned building info panels.
+            SettingsPanel.AddInfoPanelButtons();
+
+            Logging.KeyMessage("loading complete");
+
+            // Display update notification.
+            WhatsNew.ShowWhatsNew();
+
+            // Set up options panel event handler.
+            OptionsPanel.OptionsEventHook();
+        }
+    }
+}
