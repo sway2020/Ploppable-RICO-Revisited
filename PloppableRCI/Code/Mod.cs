17734d54ce8ff5393248612acec264697e6d6ab8
algernon-A <aeneas@aloku.net>
2021-03-07 20:55:36 +1100

    Prepare for 2.4.2 ALPHA 1.

--- PloppableRCI/Code/Mod.cs
+++ PloppableRCI/Code/Mod.cs
@@ -17,8 +17,8 @@ namespace PloppableRICO
         // Internal and private name and version components.
         internal static string ModName => "RICO Revisited";
         internal static string Version => BaseVersion + " " + Beta;
-        internal static string Beta => "";
-        private static string BaseVersion => "2.4.1.2";
+        internal static string Beta => "ALPHA 1";
+        private static string BaseVersion => "2.4.2";
 
 
         /// <summary>
